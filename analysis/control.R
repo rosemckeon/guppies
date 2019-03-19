88549b0a0e4cfd6558a64af33d9c4cecb409aaca
Rose McKeon <rose@rosemckeon.co.uk>
2019-03-19 19:31:05 +0000

    corrected control analysis based on reformatted data

--- analysis/control.R
+++ analysis/control.R
@@ -2,57 +2,120 @@
 if(!exists("guppies"))
   data("guppies")
 
-  # subset the data
-  control <- guppies %>%
-    filter(Predation == FALSE)
-  # fit the growth curve
-  fit_control <- nls(
-    Spot.brightness ~ a - b*exp(-c*Day),
-    data = control,
-    start = list(
-      a = 20, # asymptotic val of function (plataeu)
-      b = 16, # a - intercept at 0
-      c = 0.0039 # -log((20-14)/16)/250)
-    )
+# subset the data
+control <- guppies %>%
+  filter(Predator.number == 0)
+# fit the growth curve
+# better convergence than self starter
+# but curve is identical
+fit_control <- nls(
+  Spot.brightness ~ a - b*exp(-c*Day),
+  data = control,
+  start = list(
+    a = 20, # asymptotic val of function (plataeu)
+    b = 16, # a - intercept at 0
+    c = 0.0039 # -log((20-14)/16)/250)
   )
-  # build the plot
-  plot_control <- fit_control %>% augment() %>%
-    ggplot(
-      .,
-      aes(
-        y = Spot.brightness,
-        x = Day
-      )
-    ) + ylim(
-      0, 21 # has to include jitter range
-    ) + geom_jitter(
-      pch = guppies::roses_unicode("dot_filled"),
-      size = 1.5,
-      width = 25,
-      height = .25,
-      alpha = .3
-    ) + geom_smooth(
-      # gam method and formula matches nls fit
-      # when .fitted included in aes.
-      # cannot get nls method to compute with
-      # asymp formula
-      method = "gam",
-      formula = y ~ s(x, bs = "cs"),
-      # method = "nls",
-      # formula = y ~ a - b * exp(-c*x),
-      # method.args = list(
-      #   start = list(
-      #     a = 20,
-      #     b = 16,
-      #     c = 0.0039
-      #   )
-      # ),
-      se = F,
-      aes(
-        x = Day,
-        y = .fitted
-      ),
-      colour = "black"
-    ) + ylab(
-      "Male spot brightness"
+)
+# setup new temp x vals for predictions
+days <- seq(
+  min(control$Day),
+  max(control$Day),
+  .5
+)
+# predict new temp y vals based on model fit
+predictions <- predict(
+  fit_control,
+  newdata = list(
+    Day = days
+  ),
+  se = F
+)
+# store in dataframe for use in predation plots
+control_predictions <- data.frame(
+  X = rep(days, 3),
+  Y = rep(predictions, 3),
+  Predator.number = c(
+    rep(2, length(newx)),
+    rep(5, length(newx)),
+    rep(10, length(newx))
+  )
+)
+
+# build the plot
+plot_control <- ggplot(
+  control,
+  aes(
+    y = Spot.brightness,
+    x = Day
+  )
+) + ylim(
+  0, 21 # has to include jitter range
+) + geom_jitter(
+  pch = guppies::roses_unicode("dot_filled"),
+  size = 1.5,
+  width = 25,
+  height = .25,
+  alpha = .3
+) + geom_smooth(
+  # asymp formula
+  method = "nls",
+  formula = as.formula('y ~ a - b * exp(-c*x)'),
+  method.args = list(
+    start = list(
+      a = 20,
+      b = 10,
+      c = 0.0028
     )
+  ),
+  se = F,
+  colour = "black"
+) + ylab(
+  "Male spot brightness"
+)
+
+
+# show no difference between control factors
+control_end <- filter(control, Day > 1400)
+control_mean <- mean(control_end$Spot.brightness)
+
+# boxplot
+boxplot_control <- ggplot(
+  control_end,
+  aes(
+    y = Spot.brightness,
+    x = Substrate,
+    fill = Substrate,
+    colour = Substrate
+  )
+) + geom_jitter(
+  pch = guppies::roses_unicode("dot_filled"),
+  width = .1,
+  height = .25,
+  alpha = .5
+) + geom_boxplot(
+  colour = "black",
+  position = position_nudge(x = +0.3),
+  width = .3,
+  outlier.colour = NA
+) + scale_fill_manual(
+  values = brewer.pal(4, "Spectral")[2:4]
+) + scale_x_discrete(
+  expand = expand_scale(add = c(.3, 0.7))
+) + ylab(
+  "Male spot brightness"
+) + theme(
+  legend.position = "none",
+  axis.ticks.x = element_blank(),
+  axis.text.x=element_blank()
+) + ylim(
+  0, 21
+) + facet_grid(
+  ~Substrate,
+  space = "free",
+  scales = "free"
+)
+
+
+
+
